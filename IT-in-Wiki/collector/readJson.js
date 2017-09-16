2a83a76609dfee14b530d397de5eebdc6b29b62a
-m <zakaryan.2004@outlook.com>
2017-09-16 17:09:53 +0400

    Finished data scrapping

--- IT-in-Wiki/collector/readJson.js
+++ IT-in-Wiki/collector/readJson.js
@@ -1,50 +1,62 @@
-var fs = require('fs');
 var cheerio = require('cheerio');
 var request = require('request');
+var asyncLoop = require('node-async-loop');
 var data = require('./data/stackOverAPI.json');
-var link1 = "https://stackoverflow.com/unanswered/tagged/javascript";
-var link;
+var links = [];
+var unansweredCounts = [];
+var tagsCount = 20;
 
-function collectData(innerPage, currURL) {
-    //console.log("collectData");
-    var pageData;
-    var subTitlesArr = [];
-
+function collectData(innerPage) {
     var $ = cheerio.load(innerPage);
-
-   /* $(".summarycount al").each(function() {
-        console.log(clearData($(this).text()));
-    });*/
-
     var summarycount = $(".summarycount").text();
-    
+
     return summarycount;
 }
 
-for(i = 0; i < 7;i++)
+for(i = 0; i < tagsCount;i++)
 {
-    link = "https://stackoverflow.com/unanswered/tagged/" + data[i].name;
-    
-    request(link, function(error, response, page) { 
+    links.push("https://stackoverflow.com/unanswered/tagged/" + data[i].name);
+}
+var i = 0;
+console.log(links);
 
-            //console.log(collectData(page,link));        
-            //link = "https://stackoverflow.com/unanswered/tagged/" + data[i].name;
-            var _count = data[i].count;
-            var count = _count.toLocaleString(
-                'en-US', // use a string like 'en-US' to override browser locale
-                { minimumFractionDigits: 0 }
-              );   
-              //var answeredQuestions = parseInt(_count) - parseInt(clearData(collectData(page,link)));
-              //console.log(i+1 + ":" + data[i].name + ", "+ count + " questions" + ", "+ parseInt(_count) - parseInt(collectData(page,link)) +" answered questions");
-              //console.log(answeredQuestions);
-              console.log(link);         
-                     
-        function clearData(str) {
-            str = str.trim();
-            return str.replace(",", "");
+asyncLoop(links, function (item, next)
+{
+    i++;
+    request(item,function(error,responce,page)
+    {
+        unansweredCounts.push(collectData(page));         
+        if(i != tagsCount)
+        {
+            next();
         }
-    });
+        else
+        {
+            loop();
+        }        
+    });    
+});
+
+function loop()
+{
+    for(i = 0; i < tagsCount;i++)
+    {             
+    var _count = data[i].count;
+    var count = _count.toLocaleString(
+            'en-US', 
+            { minimumFractionDigits: 0 }
+            );   
+    var _answeredQuestions = parseInt(_count) - parseInt(clearData(unansweredCounts[i]));
+    var answeredQuestions = _answeredQuestions.toLocaleString(
+            'en-US', 
+            { minimumFractionDigits: 0 }
+            );   
+    console.log(i+1 + ":" + data[i].name + ", "+ count + " questions" + ", " + answeredQuestions + " unanswered questions");                      
+    } 
 }
 
-
-
+                
+function clearData(str) {
+    str = str.trim();
+    return str.replace(",", "");
+}
