b288f314de80bbfbcc80dbf5385086f0294edc85
zakaryan2004 <zakaryan.2004@outlook.com>
2017-12-14 13:16:16 +0400

    Data Updated

--- IT-in-Wiki/zexamples/filesystem/fs-test.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var f = "fake-data.csv";
-
-fs.appendFile(f,"Petros,Poghosyan\n");
-fs.appendFile(f,"Poghos,Martirosyan\n");
--- dataAPI/public/index.js
+++ dataAPI/public/index.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     var data;
-    google.charts.load('current', { 'packages': ['table','corechart'] });
+    var dataCount = 10;
+    google.charts.load('current', { 'packages': ['table','corechart','bar'] });
     google.charts.setOnLoadCallback(drawTable);
     google.charts.setOnLoadCallback(drawQuestionsChart);
     google.charts.setOnLoadCallback(drawAnsweredChart);
+    google.charts.setOnLoadCallback(drawQuestionBar);
+    google.charts.setOnLoadCallback(drawAnsweredPercentBar);
 
     function drawTable() {
         $.getJSON('finalData.json', function (json) {
@@ -13,7 +16,7 @@ $(document).ready(function () {
             data.addColumn('number', 'Questions');      
             data.addColumn('number','Answered Questions');
             data.addColumn('number', 'Percentage Answered');              
-            for(var i = 0;i< json.length;i++)
+            for(var i = 0;i< dataCount;i++)
             {
                 data.addRow(
                     [json[i].name,
@@ -40,21 +43,12 @@ $(document).ready(function () {
 
             data.addColumn('string', 'Name');
             data.addColumn('number', 'Questions');   
-            for(var i = 0;i< json.length;i++)
+            for(var i = 0;i< dataCount;i++)
             {
                 data.addRow(
                     [json[i].name,
-                    {v:json[i].v_questions, f:json[i].f_questions}]);
-                console.log(json[i].f_answeredQuestions);
+                    {v:json[i].v_questions, f:json[i].f_questions}]);                
             }
-            /*
-            data.addRows([
-                ['Mike', { v: 10000, f: '$10,000' }, true],
-                ['Jim', { v: 8000, f: '$8,000' }, false],
-                ['Alice', { v: 12500, f: '$12,500' }, true],
-                ['Bob', { v: 7000, f: '$7,000' }, true]
-            ]);
-            */
             var table = new google.visualization.PieChart(document.getElementById('questionChart_div'));
             table.draw(data, { legend: 'left',title: 'All Questions'});
         });
@@ -65,22 +59,57 @@ $(document).ready(function () {
             data = new google.visualization.DataTable(json);
 
             data.addColumn('string', 'Name');
-            data.addColumn('number', 'Answered Questions');      
-            //data.addColumn('number', 'Percentage Answered');              
-            for(var i = 0;i < json.length;i++)
+            data.addColumn('number', 'Answered Questions');                  
+            for(var i = 0;i < dataCount;i++)
             {
                 data.addRow([json[i].name,{v:json[i].v_answeredQuestions, f:json[i].f_answeredQuestions}]); //,{v:json[i].v_answeredPercentage, f:json[i].f_answeredPercentage + "%"}
             }
-            /*
-            data.addRows([
-                ['Mike', { v: 10000, f: '$10,000' }, true],
-                ['Jim', { v: 8000, f: '$8,000' }, false],
-                ['Alice', { v: 12500, f: '$12,500' }, true],
-                ['Bob', { v: 7000, f: '$7,000' }, true]
-            ]);
-            */
             var table = new google.visualization.PieChart(document.getElementById('answeredChart_div'));
             table.draw(data, { legend: 'left',title: 'Answered Questions'});
+        });
+    }
+    function drawQuestionBar() {
+        $.getJSON('finalData.json', function (json) {
+            data = new google.visualization.DataTable(json);
+            
+            data.addColumn('string', 'Name');
+            data.addColumn('number', 'All Questions');    
+            data.addColumn('number', 'Answered Questions');       
+            for(var i = 0;i < dataCount;i++)
+            {
+                data.addRow([json[i].name,{v:json[i].v_questions, f:json[i].f_questions},{v:json[i].v_answeredQuestions, f:json[i].f_answeredQuestions}]); //,{v:json[i].v_answeredPercentage, f:json[i].f_answeredPercentage + "%"}
+            }
+            var options = {
+                width: 800,
+                chart: {
+                  title: 'Popularity of Programming Languages'                  
+                },
+                bars: 'horizontal', // Required for Material Bar Charts.
+              };              
+            var table = new google.charts.Bar(document.getElementById('answeredBar_div'));
+            table.draw(data, options);
+        });
+    }
+
+    function drawAnsweredPercentBar() {
+        $.getJSON('finalData.json', function (json) {
+            data = new google.visualization.DataTable(json);
+            
+            data.addColumn('string', 'Name');   
+            data.addColumn('number', 'Percentage of Answered Questions'); 
+            for(var i = 0;i < dataCount;i++)
+            {
+                data.addRow([json[i].name,{v:json[i].v_answeredPercentage, f:json[i].f_answeredPercentage + "%"}]); //,{v:json[i].v_answeredPercentage, f:json[i].f_answeredPercentage + "%"}
+            }
+            var options = {
+                width: 800,
+                chart: {
+                  title: 'A peek to the percentage of Answered Questions of top Programming Languages.'                  
+                },
+                bars: 'horizontal', // Required for Material Bar Charts.
+              };              
+            var table = new google.charts.Bar(document.getElementById('answeredPercentBar_div'));
+            table.draw(data, options);
         });
     }
     /*var data;
@@ -116,5 +145,7 @@ $(window).resize(function () {
     drawQuestionsChart();
     drawAnsweredChart();
     drawTable();
+    drawQuestionBar();
+    drawAnsweredPercentBar();
 });
 });
